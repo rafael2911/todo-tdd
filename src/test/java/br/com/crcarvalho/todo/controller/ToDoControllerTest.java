a0fe6b44f727477be1527baf0a79fd158f145f35
Rafael Carvalho <crcarvalho@outlook.com>
2019-10-09 11:38:21 -0300

    Ajustes junit

--- src/test/java/br/com/crcarvalho/todo/controller/ToDoControllerTest.java
+++ src/test/java/br/com/crcarvalho/todo/controller/ToDoControllerTest.java
@@ -2,25 +2,27 @@ package br.com.crcarvalho.todo.controller;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import br.com.crcarvalho.todo.entity.ToDo;
 import br.com.crcarvalho.todo.service.ToDoService;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @WebMvcTest
 public class ToDoControllerTest {
 	
@@ -40,7 +42,9 @@ public class ToDoControllerTest {
 		
 		mockMvc.perform(MockMvcRequestBuilders.get("/todos")
 				.contentType(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk()).andDo(print());
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$", Matchers.hasSize(2)))
+		.andDo(print());
 	}
 	
 }
